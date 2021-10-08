4942d8581505acc9e0fbb2a937ccfcd1ca5f15df
hhdtc <alex_hhdtc@outlook.com>
2021-10-08 17:13:24 -0400

    add header CROS

--- src/main/java/com/acertainscientific/meetup/MeetUpApplication.java
+++ src/main/java/com/acertainscientific/meetup/MeetUpApplication.java
@@ -6,6 +6,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @MapperScan("com.acertainscientific.meetup.mapper")
@@ -17,4 +20,14 @@ public class MeetUpApplication {
 		SpringApplication.run(MeetUpApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:8089")
+													.allowedOrigins("http://us-or-cera-2.natfrp.cloud:23553");
+			}
+		};
+	}
 }
