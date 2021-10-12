9c66a2be73b59af931b3621689b7fd592e0dbef8
franchen28 <90725345+franchen28@users.noreply.github.com>
2021-10-12 17:11:35 -0400

    chensijin

---
+++ src/main/java/com/acertainscientific/meetup/service/impl/UserService.java
@@ -0,0 +1,15 @@
+package com.acertainscientific.meetup.service.impl;
+
+import com.acertainscientific.meetup.dto.*;
+import com.acertainscientific.meetup.mapper.UserMapper;
+import com.acertainscientific.meetup.model.UserModel;
+import com.acertainscientific.meetup.service.IUserService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+public class UserService {
+}
